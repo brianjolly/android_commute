4c36cd09fd08747299e7faa67edeaca21e9141f3
Brian Jolly <brian@brianjolly.com>
2011-03-04 13:55:09 -0800

    reset indentation

--- src/com/brianjolly/commute/Commute.java
+++ src/com/brianjolly/commute/Commute.java
@@ -12,89 +12,89 @@ import com.brianjolly.commute.view.BreadCrumbTrail;
 
 public class Commute extends Activity
 {
-private TextView locationText;
-private Float maxSpeed;
-private Float bestAccuracy;
-private BreadCrumbTrail breadCrumbTrail;
+    private TextView locationText;
+    private Float maxSpeed;
+    private Float bestAccuracy;
+    private BreadCrumbTrail breadCrumbTrail;
 
-/** Called when the activity is first created. */
-@Override
-public void onCreate(Bundle savedInstanceState)
-{
-  super.onCreate(savedInstanceState);
+    /** Called when the activity is first created. */
+    @Override
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
 
-  setContentView(R.layout.main);
+            setContentView(R.layout.main);
 
-  maxSpeed = 0.0f;
-  bestAccuracy = 999999999999.0f;
+            maxSpeed = 0.0f;
+            bestAccuracy = 999999999999.0f;
 
-  LocationManager locman = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+            LocationManager locman = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
-  MyLocation myLoc = new MyLocation(locman);
+            MyLocation myLoc = new MyLocation(locman);
 
-  locationText = (TextView)findViewById(R.id.location_text);
+            locationText = (TextView)findViewById(R.id.location_text);
 
-  System.out.println("about to get bc");
-	//breadCrumbTrail = (BreadCrumbTrail)findViewById(R.id.bread_crumb_trail);
-      breadCrumbTrail = new BreadCrumbTrail(this, myLoc);
+            System.out.println("about to get bc");
+            //breadCrumbTrail = (BreadCrumbTrail)findViewById(R.id.bread_crumb_trail);
+            breadCrumbTrail = new BreadCrumbTrail(this, myLoc);
 
-      System.out.println("setting model on bc");
-      //breadCrumbTrail.setModel(myLoc);
+            System.out.println("setting model on bc");
+            //breadCrumbTrail.setModel(myLoc);
 
-      myLoc.setLocationChangeListener(new MyLocation.LocationChangeListener() {
-          @Override public void onLocationChange(Location l) {
-              makeUseOfNewLocation( l );
-          } 
-      });
+            myLoc.setLocationChangeListener(new MyLocation.LocationChangeListener() {
+                @Override public void onLocationChange(Location l) {
+                    makeUseOfNewLocation( l );
+                } 
+            });
+
+        }
+
+    //public void onLocationChanged(Location location) {
+    public void makeUseOfNewLocation(Location location) {
+
+        // refresh bread crumb view
+        breadCrumbTrail.invalidate();
+
+        // refresh text view
+        if (location.getSpeed() > maxSpeed) {
+            maxSpeed = location.getSpeed();
+            Log.i("Commute", "Commute.makeUseOfNewLocation() - new max speed - " + maxSpeed);
+        }
+        if (location.getAccuracy() < bestAccuracy) {
+            bestAccuracy = location.getAccuracy();
+            Log.i("Commute", "Commute.makeUseOfNewLocation() - new best accuracy - " + bestAccuracy);
+        }
+        // at caltrain 4th&king 
+        // @37.775930,-122.394997 +/- 12.000000m
+        //
+        // at palo alto
+        // @ 37.443702, -122.165406
+        // best accuracy 3.0000m
+        // max speed 32.461445 m/s
+        //
+        //time: %tm/%td/%ty %tH:%tM
+        Location latestLocation = location;
+        String locationString = String.format(
+                "%tD %tr \n@ %f, %f \n\naccuracy +/- %fm \nbest accuracy %fm\n\naltitude %f\n\nspeed: %f m/s\nspeed: %f mph\nmax speed: %f m/s\nmax speed: %f mph",
+                location.getTime(),
+                location.getTime(),
+                location.getLatitude(),
+                location.getLongitude(),
+                location.getAccuracy(),
+                bestAccuracy,
+                location.getAltitude(),
+                location.getSpeed(),
+                speedInMPH(location.getSpeed()),
+                maxSpeed,
+                speedInMPH(maxSpeed)
+                );
+        locationText.setText(locationString);
 
     }
 
-  //public void onLocationChanged(Location location) {
-  public void makeUseOfNewLocation(Location location) {
-
-    // refresh bread crumb view
-    breadCrumbTrail.invalidate();
-
-    // refresh text view
-    if (location.getSpeed() > maxSpeed) {
-      maxSpeed = location.getSpeed();
-      Log.i("Commute", "Commute.makeUseOfNewLocation() - new max speed - " + maxSpeed);
+    public Float speedInMPH( Float speedInMetersPerSec ) {
+        //1 meter per second = 2.23693629 miles per hour
+        return speedInMetersPerSec * 2.23693629f;
     }
-    if (location.getAccuracy() < bestAccuracy) {
-      bestAccuracy = location.getAccuracy();
-      Log.i("Commute", "Commute.makeUseOfNewLocation() - new best accuracy - " + bestAccuracy);
+
     }
-    // at caltrain 4th&king 
-    // @37.775930,-122.394997 +/- 12.000000m
-    //
-    // at palo alto
-    // @ 37.443702, -122.165406
-    // best accuracy 3.0000m
-    // max speed 32.461445 m/s
-    //
-    //time: %tm/%td/%ty %tH:%tM
-    Location latestLocation = location;
-    String locationString = String.format(
-        "%tD %tr \n@ %f, %f \n\naccuracy +/- %fm \nbest accuracy %fm\n\naltitude %f\n\nspeed: %f m/s\nspeed: %f mph\nmax speed: %f m/s\nmax speed: %f mph",
-        location.getTime(),
-        location.getTime(),
-        location.getLatitude(),
-        location.getLongitude(),
-        location.getAccuracy(),
-        bestAccuracy,
-        location.getAltitude(),
-        location.getSpeed(),
-        speedInMPH(location.getSpeed()),
-        maxSpeed,
-        speedInMPH(maxSpeed)
-        );
-    locationText.setText(locationString);
-
-  }
-
-  public Float speedInMPH( Float speedInMetersPerSec ) {
-    //1 meter per second = 2.23693629 miles per hour
-    return speedInMetersPerSec * 2.23693629f;
-  }
-
-}
